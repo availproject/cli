967a4b2b4a371ce9df86d12ca9454005e8e4fc23
QEDK <1994constant@gmail.com>
2023-11-09 20:03:09 +0400

    feat: init

---
+++ index.ts
@@ -0,0 +1,60 @@
+import { Command, Option } from 'commander';
+const program = new Command();
+import { getDecimals, initialize, formatNumberToBalance, getKeyringFromSeed, isValidAddress } from "avail-js-sdk";
+
+enum NetworkNames {
+    Kate = 'kate',
+    Goldberg = 'goldberg',
+}
+
+const NETWORK_RPC_URLS: { kate: string, goldberg: string } = {
+    'kate': 'wss://kate.avail.tools/ws',
+    'goldberg': 'wss://goldberg.avail.tools/ws',
+}
+
+program
+    .name('avail-cli')
+    .description('A simple CLI for Avail network utilities')
+    .version('0.1.0');
+
+const transfer = async (options: {
+    seed: string,
+    network: NetworkNames,
+    rpc: string,
+    to: string,
+    value: number,
+}) => {
+    try {
+        const seed = options!.seed
+        const recipient = options!.to
+
+        if (!isValidAddress(recipient)) throw new Error(recipient + " recipient address is invalid")
+
+        let rpcUrl: string;
+        if (typeof (NETWORK_RPC_URLS[options.network]) === 'undefined') {
+            rpcUrl = options.rpc
+        } else {
+            rpcUrl = NETWORK_RPC_URLS[options.network]
+        }
+        const api = await initialize(rpcUrl, { noInitWarn: true })
+        const keyring = getKeyringFromSeed(seed)
+        const amount = formatNumberToBalance(options.value)
+
+        await api.tx.balances.transfer(recipient, amount).signAndSend(keyring, { nonce: -1 })
+        process.exit(0);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+program
+    .command('transfer')
+    .addOption(new Option('-n --network <network name>', 'network name').choices(['kate', 'goldberg']).default('goldberg').conflicts('rpc'))
+    .addOption(new Option('-r, --rpc <RPC url>', 'the RPC url to connect to').env('AVAIL_RPC_URL').default(NETWORK_RPC_URLS['goldberg']))
+    .addOption(new Option('-s, --seed <seed phrase>', 'the seed phrase for the Avail account').env('AVAIL_SEED').makeOptionMandatory())
+    .requiredOption('--to <recipient address>', 'the recipient address')
+    .requiredOption('--value <amount in AVL>', 'the value in AVL (10e18) to transfer')
+    .action(transfer)
+
+program.parse();
